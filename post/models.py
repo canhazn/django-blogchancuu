275436a5b6b5b6e4ab2cd8794b77a47daf04e551
tuan anh <canhazn@gmail.com>
2020-05-15 10:40:57 +0700

    done sulg

--- post/models.py
+++ post/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import User
+from django_unique_slugify import unique_slugify
+from django.utils import timezone
 
 STATUS = (
 	(0, "Draft"),
@@ -11,11 +13,12 @@ STATUS = (
 
 class Post(models.Model): 
 	title = models.CharField(max_length=200, null=True, blank=True)
-	slug = models.SlugField(max_length=200, unique=True)
+	slug  = models.SlugField(max_length=200, null=True, blank=True)	
 	# author = models.ForeignKey(User, on_delete= models.CASCADE, related_name="blog_posts")
 	update_on = models.DateTimeField(auto_now= True)
 	content = models.TextField()
-	created_on = models.DateTimeField(auto_now_add=True)
+	created_on = models.DateTimeField(auto_now_add= True)
+	# created_on = models.DateTimeField(editable=False, default=timezone.now)
 	status = models.IntegerField(choices= STATUS, default=1)
 
 	class Meta:
@@ -23,6 +26,17 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.slug
+
+	def save(self, **kwargs):
+		# check because update post can overwrite slug
+		if not self.slug:
+			if self.title:
+				slug_str = "%s" % (self.title) 
+			else:
+				slug_str = "%s" % (timezone.now()) 				
+			unique_slugify(self, slug_str) 
+		
+		super(Post, self).save(**kwargs)
 
 class Comment(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
