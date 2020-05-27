478b6d01597f21f470037fd8ede7c2cb5c791ccc
tuan anh <canhazn@gmail.com>
2020-05-27 12:16:21 +0700

    ok

--- blogchancuu/settings.py
+++ blogchancuu/settings.py
@@ -131,8 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/blogchancuu-bucket/static_root/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT= os.path.join(BASE_DIR,'static_root/')
 
 # Media paths
 # Base url to serve media files
