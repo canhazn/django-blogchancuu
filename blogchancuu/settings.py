2cde7aea52232af79dd227912283916ec052bb24
tuan anh <canhazn@gmail.com>
2020-05-27 13:16:57 +0700

    ok

--- blogchancuu/settings.py
+++ blogchancuu/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-1+gysmfpjc_9k!j4vh4)52qh6wpqwdlq+l8%wmv0jbhw&k!58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
